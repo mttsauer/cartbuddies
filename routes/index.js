fd84b5fc64ea86980f416c268df788346c84f8b3
Matt Sauer <MattESauer@gillettechildrens.com>
2018-08-19 12:25:56 -0500

    Enabled parameter passthrough to form

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Start a Cart' });
+  res.render('index', { title: 'Start a Cart', website: req.param('website'), product: req.param('product'), size: req.param('size') });
 });
 
 router.get('/x:command', function(req, res, next) {
